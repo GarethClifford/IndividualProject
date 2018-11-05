3768e759279210070f88224d9a8da0e578f7ef23
Gareth <gareth.clifford@live.co.uk>
2018-11-05 16:31:29 +0000

    Working

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,4 +1,5 @@
-INSERT INTO TRAINER (trainerName) VALUES ('John Gordon');
-INSERT INTO TRAINER (trainerName) VALUES ('Matt Hunt');
-INSERT INTO TRAINEE (trainerName, trainerID) VALUES ('Gareth Clifford',1);
-INSERT INTO TRAINEE (trainerName, trainerID) VALUES ('Andy Nutts',2);
+INSERT INTO TRAINER (classroomID, trainer) VALUES (1, 'John Gordon');
+INSERT INTO TRAINER (classroomID, trainer) VALUES (2, 'Matt Hunt');
+INSERT INTO TRAINEE (traineeName, classroomID) VALUES ('Gareth Clifford',1);
+INSERT INTO TRAINEE (traineeName, classroomID) VALUES ('Andy Knott',1);
+INSERT INTO TRAINEE (traineeName, classroomID) VALUES ('Ben Tayor',2);
